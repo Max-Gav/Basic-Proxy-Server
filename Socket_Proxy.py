02655cbc82dfb4140f3d6a4c4809e514ccaa6c46
Maxim Gavrilove <maximgav123@gmail.com>
2024-03-27 16:16:13 +0200

    Used multiplexing and selectors

--- Socket_Proxy.py
+++ Socket_Proxy.py
@@ -1,3 +1,4 @@
+import selectors
 import socket
 from multiprocessing import cpu_count, Manager
 from threading import Thread
@@ -9,30 +10,37 @@ server_host = "127.0.0.1"
 server_port = 8801
         
 def clientRequestReceiver(client_socket, request_queue):
-    while True:
-        request_data = client_socket.recv(buff_size).decode()
-        
-        if request_data is None or request_data == "EXIT":
-            print("Client disconnected")
-            break
-        print("Proxy received request from client: "+request_data)
-        
-        request_queue.put((request_data, client_socket))
-        
-    client_socket.close()
-
+    request_data = client_socket.recv(buff_size).decode()
+            
+    if request_data is None or request_data == "EXIT":
+        print("Client disconnected")
+        client_socket.close()
+                
+    print("Proxy received request from client: "+request_data)
+            
+    request_queue.put((request_data, client_socket))
+     
 def mainProcessHandler(request_queue):
+    selector = selectors.DefaultSelector()
     proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     proxy_socket.bind(("0.0.0.0", 8800))
     proxy_socket.listen(5)
+    selector.register(proxy_socket, selectors.EVENT_READ)
     print("Started proxy server")
-    
-    with ThreadPoolExecutor(max_workers=5) as request_receiver_thread_pool:        
-        while True:
-            print("Waiting for client connection")
-            client_socket, client_address = proxy_socket.accept()
-            print("Accepted a client connection from " + client_address[0] + ":" + str(client_address[1]))
-            request_receiver_thread_pool.submit(clientRequestReceiver, client_socket, request_queue)
+
+    while True:
+        events = selector.select()
+
+        for key, mask in events:
+            current_socket = key.fileobj
+
+            if current_socket == proxy_socket:
+                client_socket, client_address = current_socket.accept()
+                print("Accepted a client connection from " + client_address[0] + ":" + str(client_address[1]))
+                selector.register(client_socket, selectors.EVENT_READ, data=client_address)
+            else:
+                print("Handling request for "+ key.data[0] + ":" + str(key.data[1]))
+                clientRequestReceiver(current_socket, request_queue)
             
 def sendClientRequest(request_data, client_socket, remote_socket):
     remote_socket.send(request_data.encode())
@@ -42,7 +50,7 @@ def sendClientRequest(request_data, client_socket, remote_socket):
 
     client_socket.send(response_data.encode())
 
-def handleRequest(remote_socket, request_queue):
+def listenToRequestProcessor(remote_socket, request_queue):
     print("Initilaized processor")
 
     while True:
@@ -56,7 +64,7 @@ def startRequestHandlersProcessors(request_queue):
     
     with ProcessPoolExecutor(max_workers=proccesors_num) as processor_pool:         
         for i in range(proccesors_num - 1):
-            processor_pool.submit(handleRequest, remote_socket, request_queue)
+            processor_pool.submit(listenToRequestProcessor, remote_socket, request_queue)
         
 def startProxyServer():
     request_queue = Manager().Queue()
